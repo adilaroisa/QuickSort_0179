40c2aac630ed5a347156666c97f8ab959f2e9450
adilaroisa <adilaroisa688@gmail.com>
2024-03-28 10:07:13 +0700

    membuat untuk mencari elemen yang lebih besar dari pivot

--- AlgoritmaQuickSort28032024/AlgoritmaQuickSort28032024.cpp
+++ AlgoritmaQuickSort28032024/AlgoritmaQuickSort28032024.cpp
@@ -49,6 +49,16 @@ void q_sort(int low, int high)
 	pivot = arr[low]; // step 2
 	i = low + 1; // step 3
 	j = high; // step 4
+
+	while (i <= j) // step 10
+	{
+		// search for an element greater that pivot
+		while ((arr[i] <= pivot) && (j >= low)) // step 7
+		{
+			i++; // step 6
+			cmp_count++;
+		}
+	}
 }
 
 int main()
